961ef0ba8bcd8c62957ab8d2e61a33b1417ad6a9
Bradley Evans <bevan006@ucr.edu>
2017-04-20 13:56:19 -0700

    Pulled down diegorusso's version of the vmcreator for testing.

--- vmcreator/README.md
+++ vmcreator/README.md
@@ -1,20 +1,52 @@
 This script creates the team and organization VMs.
 
-Usage:
-  
-    create_vm.py <game_hash>
+    usage: create_vms.py [-h] [--hash GAME_HASH] [--json GAME_JSON]
+                         [-o OUTPUT_PATH] [--log LOG_PATH]
 
-It runs on our web site (http://ictf.cs.ucsb.edu/framework/#/framework): it retrieves the JSON configuration for `game_hash` and generates the VMs for it. Once generated, the VMs are independent from our web site.
+    optional arguments:
+        -h, --help        show this help message and exit
+        --hash GAME_HASH
+        --json GAME_JSON
+        -o OUTPUT_PATH
+        --log LOG_PATH
 
-Besides the JSON configuration, it needs:
+It can run either on our web site (http://ictf.cs.ucsb.edu/framework/#/framework) or locally.
+ * If the option --hash is specified, tt retrieves the JSON configuration for `GAME_HASH` and generates the VMs for it.
+ * If the option --json is specified, it reads the file `GAME_JSON` and generates the VMs for it.
 
- - the base VMs as setup (base Ubuntu with no resolvconf and preinstalled dependencies - you can get ours at http://ictf.cs.ucsb.edu/base-vms/base-vms.tar.gz)
- - **sudo access** (so it can guestmount and chroot)
- - the services as `/services/name.deb` and `/services/name/info.json` (and their scripts)
- - the organization VM setup scripts as `/org/` (see create\_vms.py)
- - ability to write in `/game/` and `/var/www/bundles`
- - ability to use qemu-kvm
- - libguestfs-tools (run 'update-guestfs-appliance' if necessary)
- - virtualbox (VBoxManage)
+If the script runs remotely, the VMs can be downloaded from our website
+If the script runs locally, the bundle is created in `OUTPUT_PATH`
+
+Once generated, the VMs are independent from our web site.
+
+# Download and untar the base VMs: base Ubuntu with no resolvconf and preinstalled dependencies
+wget http://ictf.cs.ucsb.edu/base-vms/base-vms.tar.gz
+tar xzvf base-vms.tar.gz
+
+# Setup the environment to create the VMs (as root)
+ln -s /path/to/services /ictf
+ln -s /path/to/database /ictf/
+ln -s /path/to/dashboard/ /ictf/
+ln -s /path/to/scorebot/ /ictf/
+
+# Install required packages (as root)
+apt-get install libguestfs-tools virtualbox
+update-guestfs-appliance
+
+# Register the base VMs with VirtualBox (as root)
+VBoxManage registervm /path/to/base-vms/iCTF-base/iCTF-base.vbox
+VBoxManage registervm /path/to/base-vms/Organization-base/Organization-base.vbox
+VBoxManage list vms
+
+# Create VMs described by example\_game.json
+sudo python create\_vms.py --json example\_game.json -o /path/to/store/bundle
+
+# Configure port forwarding to ssh the ORG VM
+Before starting the ORG VM, type on your host:
+VBoxManage modifyvm Organization --natpf2 "ssh,tcp,,2222,,22"
+
+# Start the ORG VM and then ssh to it:
+ssh -p 2222 root@127.0.0.1 -l /path/to/root\_key
 
 *Note:* the VMs, as generated, use a single VirtualBox internal network. Test it, then setup a real game network using the documentation in the `router/` directory.
+*Note2:* if you are generating VMs in a Virtual Machine running on Virtual Box, don't specify -o option in a shared directory: this will corrupt the VMs generated.
---
+++ vmcreator/utils.py
@@ -0,0 +1,255 @@
+import re
+import sys
+import os
+import json
+import subprocess
+import tempfile
+import logging
+import argparse
+import hashlib
+import requests
+import shutil
+import base64
+
+
+class ArgParser(object):
+    def parse(self, argv):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--hash', dest='game_hash', action='store')
+        parser.add_argument('--json', dest='game_json', action='store')
+        parser.add_argument('-o', dest='output_path', action='store')
+        parser.add_argument('--log', dest='log_path', action='store',
+                            default='/tmp')
+        args = parser.parse_args(argv)
+        if not args.output_path:
+            parser.error('Please specify a path where to store the bundle')
+        if args.game_hash:
+            args.remote = True
+            assert re.match(r'[a-zA-Z0-9]+\Z', args.game_hash)
+            req_str = "http://ictf.cs.ucsb.edu/framework/ctf/{}"
+            game = requests.get(req_str.format(args.game_hash)).json()
+            args.game = game
+            sys.path.insert(0, "/var/www/framework")
+            import secrets
+        elif args.game_json:
+            args.remote = False
+            try:
+                json_game = open(args.game_json, 'r')
+                game = json.load(json_game)
+                json_game.close()
+                args.game_hash = hashlib.md5(game.get('name')).hexdigest()
+                args.game = game
+            except Exception, e:
+                raise Exception(e)
+        else:
+            parser.error('Please specify either a hash (remote) or a json '
+                         'descriptor (local)')
+        return args
+
+
+def status(game_hash, status_msg, remote=False):
+    logging.info("Status: {}".format(status_msg))
+    # Post status only if it remote
+    if remote and status_msg in ['READY', 'CONFIRMED', 'PENDING', 'ERROR']:
+        post = "http://ictf.cs.ucsb.edu/framework/ctf/status/{}/{}?secret={}"
+        requests.post(post.format(game_hash, status_msg, secrets.API_SECRET))
+
+
+def run_cmd(arglist, cmd_name):
+    logging.debug("Running {} ({})".format(cmd_name, ' '.join(arglist)))
+    try:
+        out = subprocess.check_output(arglist, stderr=subprocess.STDOUT)
+        logging.debug("\tOutput: {}".format(out))
+    except subprocess.CalledProcessError as e:
+        logging.error("")
+        logging.error(" **** {} failed **** ".format(cmd_name))
+        logging.error(" ** Command line: {}".format(e.cmd))
+        logging.error(" ** Return code: {}".format(e.returncode))
+        logging.error(" ** Output: {}".format(e.output))
+        logging.error(" **************************************************")
+        logging.error("")
+        raise
+
+
+def gamepath(output_path, game_hash):
+    path = os.path.join(output_path, game_hash)
+    try:
+        os.makedirs(path)
+    except OSError:
+        if not os.path.isdir(path):
+            raise
+    return path
+
+
+def clean_up(output_path, game_hash, teams=None, bundle=False):
+    game_path = gamepath(output_path, game_hash)
+    if os.path.exists(game_path):
+        logging.info('Removing game dir {}'.format(game_path))
+        shutil.rmtree(game_path)
+    if bundle:
+        bundle_game_path = '{}.tar.gz'.format(game_path)
+        if os.path.exists(bundle_game_path):
+            logging.info('Removing bundle {}'.format(bundle_game_path))
+            os.remove(bundle_game_path)
+        for team in teams:
+            bundle_team_path = '{}.tar.gz'.format(os.path.join(output_path,
+                                                               team['name']))
+            if os.path.exists(bundle_team_path):
+                logging.info('Removing bundle {}'.format(bundle_team_path))
+                os.remove(bundle_team_path)
+
+
+def mountdir_copyfile(mntdir, frompath, topath):
+    run_cmd(['sudo', 'cp', '-f', frompath, '{}/{}'.format(mntdir, topath)],
+            'cp file to mounted dir')
+
+
+def mountdir_copydir(mntdir, frompath, topath):
+    run_cmd(['sudo', 'cp', '-a', frompath, '{}/{}'.format(mntdir, topath)],
+            'cp path to mounted dir')
+
+
+def mountdir_writefile(mntdir, topath, content):
+    tmpfd, tmpfilename = tempfile.mkstemp(prefix='formnt_')
+    tmpfile = os.fdopen(tmpfd, 'w')
+    tmpfile.write(content)
+    tmpfile.close()
+    mountdir_copyfile(mntdir, tmpfilename, topath)
+    os.unlink(tmpfilename)
+
+
+def mountdir_appendfile(mntdir, topath, content):
+    orig_file = open(os.path.join(mntdir, topath), 'r')
+    file_content = orig_file.read()
+    orig_file.close()
+    new_content = '{}{}'.format(file_content, content)
+    mountdir_writefile(mntdir, topath, new_content)
+
+
+def mountdir_bash(mntdir, bash_cmd):
+    run_cmd(['sudo', 'chroot', mntdir, '/bin/bash', '-c', bash_cmd],
+            "cmd in mounted dir")
+
+
+def mountdir_start_config(mntdir, ip, netmask, hostname, root_key, team_key,
+                          has_nat=False):
+    # Network config
+    interfaces = """
+auto lo
+iface lo inet loopback
+auto eth0
+iface eth0 inet static
+    address {}
+    netmask {}
+""".format(ip, netmask)
+    if has_nat:
+        interfaces += '\nauto eth1\niface eth1 inet dhcp\n'
+    mountdir_writefile(mntdir, '/etc/network/interfaces', interfaces)
+    mountdir_writefile(mntdir, '/etc/hostname', hostname)
+    hosts = """
+127.0.0.1   localhost {}
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+""".format(hostname)
+    mountdir_writefile(mntdir, '/etc/hosts', hosts)
+
+    # Login config
+    # Note: This does _not_ prevent console logins with the default password.
+    #       If you give out the VM, players can always get root.
+    mountdir_bash(mntdir, 'mkdir -p /root/.ssh')
+    mountdir_writefile(mntdir, '/root/.ssh/authorized_keys', root_key)
+    mountdir_bash(mntdir, 'mkdir -p /home/ctf/.ssh')
+    mountdir_bash(mntdir, 'chown ctf:ctf /home/ctf/.ssh')
+    mountdir_bash(mntdir, 'chmod go-rwx /home/ctf/.ssh')
+    mountdir_writefile(mntdir, '/home/ctf/.ssh/authorized_keys', team_key)
+    mountdir_bash(mntdir, 'chown ctf:ctf /home/ctf/.ssh/authorized_keys')
+    mountdir_bash(mntdir, 'chmod go-rwx /home/ctf/.ssh/authorized_keys')
+    mountdir_writefile(mntdir, '/etc/ssh/sshd_config', """
+Port 22
+Protocol 2
+HostKey /etc/ssh/ssh_host_rsa_key
+HostKey /etc/ssh/ssh_host_dsa_key
+HostKey /etc/ssh/ssh_host_ecdsa_key
+HostKey /etc/ssh/ssh_host_ed25519_key
+UsePrivilegeSeparation yes
+SyslogFacility AUTH
+LogLevel INFO
+LoginGraceTime 120
+StrictModes yes
+RSAAuthentication yes
+PubkeyAuthentication yes
+IgnoreRhosts yes
+RhostsRSAAuthentication no
+HostbasedAuthentication no
+PermitEmptyPasswords no
+TCPKeepAlive yes
+AcceptEnv LANG LC_*
+Subsystem sftp /usr/lib/openssh/sftp-server
+UsePAM yes
+PermitRootLogin yes
+ChallengeResponseAuthentication no
+PasswordAuthentication no
+UseDNS no
+""")
+
+    # General reset
+    mountdir_bash(mntdir, "rm -rf /var/cache/apt/*")
+    mountdir_bash(mntdir, "rm -f /etc/ssh/ssh_host*")
+    mountdir_writefile(mntdir, '/etc/rc.local', """#!/bin/sh -e
+dpkg-reconfigure openssh-server
+exit 0""")
+    mountdir_bash(mntdir, "chmod a+x /etc/rc.local")
+
+    # Set a DNS server that works when chrooted in
+    #     (mountdir_end_config will set the real one)
+    mountdir_bash(mntdir, 'rm -f /etc/resolv.conf')
+    mountdir_writefile(mntdir, '/etc/resolv.conf', 'nameserver 8.8.8.8')
+
+
+def mountdir_end_config(mntdir):
+    # No DNS server in this version, unless provided by dhcp
+    mountdir_writefile(mntdir, '/etc/resolv.conf', '')
+
+
+def mountdir_install_deb(mntdir, deb_path):
+    deb_filename = os.path.basename(deb_path)
+    mountdir_copyfile(mntdir, deb_path, '/opt/{}'.format(deb_filename))
+    # This is not possible to execute here because some services needs a live system
+    # Once the VM is created and blundled, ssh into them and install the packages
+    # mountdir_bash(mntdir, "gdebi -q -n /{}".format(deb_filename))
+
+
+def create_ssh_key(secret_file_name):
+    run_cmd(['ssh-keygen', '-q', '-N', '', '-C',
+             os.path.basename(secret_file_name), '-f', secret_file_name],
+            "ssh-keygen")
+    with open('{}.pub'.format(secret_file_name)) as public_key_file:
+        return public_key_file.read()
+
+
+def read_service_script(path):
+    with open(path) as script_file:
+        return base64.b64encode(script_file.read())
+
+
+def bundle(game_hash, vm_name, key_name, team_hash, output_dir, remote):
+    status(game_hash, "Creating the {} bundle".format(vm_name), remote)
+    game_dir = gamepath(output_dir, game_hash)
+    os.chdir(game_dir)
+    files = ["{}/{}.vbox".format(vm_name, vm_name),
+             "{}/{}-disk1.vdi".format(vm_name, vm_name),
+             key_name,
+             "{}.pub".format(key_name)]
+    for bundle_file in files:
+        assert os.path.isfile(bundle_file)
+    bundle_filename = "{}.tar.gz".format(team_hash)
+    logging.info("Putting the {} bundle in {}".format(vm_name,
+                                                      bundle_filename))
+    tar_cmdline = ['tar', 'caf', os.path.join(output_dir, bundle_filename)]
+    tar_cmdline.extend(files)
+    run_cmd(tar_cmdline, "bundle tar")
+    status(game_hash, "Created the {} bundle".format(vm_name), remote)
+    return bundle_filename
